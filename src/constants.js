34e99f4bd6a59d488efefac56fbeb80feaa423db
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-06-25 17:56:17 +0200

    añadido enlace

--- src/constants.js
+++ src/constants.js
@@ -8,7 +8,7 @@ export const dialogueData = {
     llave: `Llévatela, es una herramienta que quizás uses más tarde.`,
     rana: `Soy malo, y eso es bueno. Yo jamás seré bueno y eso no es malo. No hay nadie quien quiera ser además de mí.`,
     personaje: `Estas a solo un paso de alcanzar la gloria, o tus regalos, jeje. Para ello, ten en cuenta, que no es solo por tu cumpleaños, si no por el paso tan importante que dareis el próximo 8 de marzo. Pide ayuda a alguien para desvelar que...`,
-    castillo: ``,
+    castillo: `Tu regalo está justo <a href="https://cuentaatrasparadisneymartirio.netlify.app/" target="_blank">aquí</a>`,
     exit: `Si quieres salir, solo tienes que cerrar la pestaña`,
   };
   
