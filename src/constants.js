755d88b2160f6a39b8b3c29a9ed2c57fc96b088f
Francisco Estepa Guerra <estepaguerrafrancisco@gmail.com>
2024-06-25 16:25:55 +0200

    juego-marta

---
+++ src/constants.js
@@ -0,0 +1,14 @@
+export const scaleFactor = 4
+
+export const dialogueData = {
+    mago: `Bienvenida a en un mundo, el juego donde todo puede ser..o no. Ve buscando información en todos los objetos y criaturas, puede que te sean de ayuda..o no. Jeje`,
+    pocion: `En un mundo donde la pocion roja te da el super poder que quieras y la pocion verde te da el sueldo nescafe, ¿que poción beberias?`,
+    moneda: `Parece que a alguien se le ha caido esta moneda, posiblemente sea de algún tesoro, guardala por si acaso.`,
+    cofre: `Has cogido la llave incorrecta, era la que tiene un aspecto viejo, oxidada como la cerradura, con un ala rota.`,
+    llave: `Llévatela, es una herramienta que quizás uses más tarde.`,
+    rana: `Soy malo, y eso es bueno. Yo jamás seré bueno y eso no es malo. No hay nadie quien quiera ser además de mí.`,
+    personaje: `Estas a solo un paso de alcanzar la gloria, o tus regalos, jeje. Para ello, ten en cuenta, que no es solo por tu cumpleaños, si no por el paso tan importante que dareis el próximo 8 de marzo. Pide ayuda a alguien para desvelar que...`,
+    castillo: ``,
+    exit: `Si quieres salir, solo tienes que cerrar la pestaña`,
+  };
+  
